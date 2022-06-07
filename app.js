b7f47748c9c0afab1f613ecc2a5333514af0ce50
Douglas Freitas <freitas28douglas@gmail.com>
2022-06-07 17:38:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,25 +5,42 @@ const app = express();
 app.use(express.json());
 
 app.post("/contas-a-receber", (req, res) => {
-    const response = {
-        descricao: req.body.descricao,
-        valor: req.body.valor,
-        datadecompetencia: req.body.datadecompetencia,
-        datadecaixa: req.body.datadecaixa,
-        contato: req.body.contato
-    };
+    
+    if(!req.body.descricao || typeof req.body.descricao == undefined || req.body.descricao == null ) {
+        res.status(400).json({message: "Descrição Inválida"})
+    }
+    
+    if(!req.body.valor || typeof req.body.valor == undefined || req.body.valor == null ) {
+        res.status(400).json({message: "Valor Inválido"})
+    }
+    
+    if(!req.body.datadecompetencia || typeof req.body.datadecompetencia == undefined || req.body.datadecompetencia == null ) {
+        res.status(400).json({message: "Data Inválida"})
+    }
+        
+    if(!req.body.datadecaixa || typeof req.body.datadecaixa == undefined || req.body.datadecaixa == null ) {
+        res.status(400).json({message: "Descrição Inválida"})
+    }    
+    
+    if(!req.body.contato || typeof req.body.contato == undefined || req.body.contato == null ) {
+        res.status(400).json({message: "Contato Inválido"})
+    } else {
+        const response = {
+            descricao: req.body.descricao,
+            valor: req.body.valor,
+            datadecompetencia: req.body.datadecompetencia,
+            datadecaixa: req.body.datadecaixa,
+            contato: req.body.contato
+        };
 
-    fs.writeFileSync("teste.json", JSON.stringify(response))
-    return res.send("Conta salva com sucesso!");
-
+        fs.writeFileSync("teste.json", JSON.stringify(response))
+        return res.status(200).json({message:"Conta salva com sucesso!"});
+    }
 });
 
 app.get("/contas-a-receber/relatorios", (req, res) => {
-    fs.readFile('teste.json', (err, arquivo) => {
-        res.writeHead(200, { 'Content-Type': 'text/txt' });
-        res.write(arquivo);
-
-        return res.end();
+    fs.readFileSync('teste.json', (err, arquivo) => {
+        return res.status(200).json({message: "Erro."})
     });
 })
 
