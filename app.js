9b5ec29cf35e1b546fb5ab8d0638a8de3e416559
Douglas Freitas <freitas28douglas@gmail.com>
2022-06-14 17:32:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ app.use(express.json());
 
 app.post("/contas-a-receber", (req, res) => {
     
-    if(!req.body.descricao || typeof req.body.descricao == undefined || req.body.descricao == null ) {
-        res.status(400).json({message: "Descrição Inválida"})
+    if(!req.body.descricao) {
+        return res.status(400).json({message: "A descrição da conta não foi informada, verifique" })
     }
     
-    if(!req.body.valor || typeof req.body.valor == undefined || req.body.valor == null ) {
-        res.status(400).json({message: "Valor Inválido"})
+    if(!req.body.valor ) {
+        return res.status(400).json({message: "O valor da conta não foi informado, verifique"})
     }
     
-    if(!req.body.datadecompetencia || typeof req.body.datadecompetencia == undefined || req.body.datadecompetencia == null ) {
-        res.status(400).json({message: "Data Inválida"})
+    if(!req.body.data_de_competencia) {
+        return res.status(400).json({message: "A data da conta não foi informada, verifique"})
     }
         
-    if(!req.body.datadecaixa || typeof req.body.datadecaixa == undefined || req.body.datadecaixa == null ) {
-        res.status(400).json({message: "Descrição Inválida"})
+    if(!req.body.data_de_caixa) {
+        return res.status(400).json({message: "A descrição da conta não foi informada, verifique"})
     }    
     
-    if(!req.body.contato || typeof req.body.contato == undefined || req.body.contato == null ) {
-        res.status(400).json({message: "Contato Inválido"})
-    } else {
+    if(!req.body.contato) {
+        return res.status(400).json({message: "O contato da conta não foi informada, verifique"})
+    }  
         const response = {
             descricao: req.body.descricao,
             valor: req.body.valor,
@@ -34,8 +34,8 @@ app.post("/contas-a-receber", (req, res) => {
         };
 
         fs.writeFileSync("teste.json", JSON.stringify(response))
-        return res.status(200).json({message:"Conta salva com sucesso!"});
-    }
+        return res.status(200).json({message: "Conta salva com sucesso!" });
+    
 });
 
 app.get("/contas-a-receber/relatorios", (req, res) => {
@@ -43,7 +43,7 @@ app.get("/contas-a-receber/relatorios", (req, res) => {
     fs.readFile('teste.json', (err, arquivo) => {
         
         if(err) {
-            return res.status(400).json({message:"Erro"})
+            return res.status(400).json({message: "Erro" })
         };
 
         return res.status(200).json(arquivo);
