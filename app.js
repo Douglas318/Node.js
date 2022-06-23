d63b3e2da2660450e4a978fba8497916d00f3141
Douglas Freitas <freitas28douglas@gmail.com>
2022-06-23 17:32:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,36 +4,42 @@ const app = express();
 
 app.use(express.json());
 
+//Não terminei
 app.post("/contas-a-receber", (req, res) => {
-  
-    if(!req.body.descricao) {
+try {
+    
+    if (!req.body.descricao) {
         return res.status(400).json({message: "A descrição da conta não foi informada, verifique" })
     } 
-    if(!req.body.valor ) {
+    if (!req.body.valor) {
         return res.status(400).json({message: "O valor da conta não foi informado, verifique"})
     }
-    if(!req.body.data_de_competencia) {
+    if (!req.body.data_de_competencia) {
         return res.status(400).json({message: "A de competencia não foi informada, verifique"})
     }
-    if(!req.body.data_de_caixa) {
+    if (!req.body.data_de_caixa) {
         return res.status(400).json({message: "A data de caixa não foi informada, verifique"})
     }    
-    if(!req.body.contato) {
+    if (!req.body.contato) {
         return res.status(400).json({message: "O contato da conta não foi informada, verifique"})
-} 
+    } 
 
     const response = {
-    descricao: req.body.descricao,
-    valor: req.body.valor,
-    data_de_competencia: req.body.data_de_competencia,
-    data_de_caixa: req.body.data_de_caixa,
-    contato: req.body.contato
+        descricao: req.body.descricao,
+        valor: req.body.valor,
+        data_de_competencia: req.body.data_de_competencia,
+        data_de_caixa: req.body.data_de_caixa,
+        contato: req.body.contato
     };
 
     fs.writeFile('teste.json', JSON.stringify(response, null, 2), (err) =>{
         if (err) throw err;
         return res.status(200).json({message: "Conta salva com sucesso!"})
     })
+
+}   catch (error) {
+        return res.status(500).json({message: "Erro ao tentar salvar o arquivo"})
+    }
 
 });
 
@@ -47,6 +53,5 @@ app.get("/contas-a-receber/relatorios", (req, res) => {
 });
 
 app.listen(8083, () => {
-    console.log("Servidor rodando http://localhost:8083")
+        console.log("Servidor rodando http://localhost:8083")
 })
-
