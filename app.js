57025ab71c4d0fa8092caf3d552bfba3b702beaf
Douglas Freitas <freitas28douglas@gmail.com>
2022-07-14 16:51:59 -0300

    refactor:
    Alterações de revisão de código

--- app.js
+++ app.js
@@ -4,25 +4,24 @@ const app = express();
 
 app.use(express.json());
 
-//Não terminei
 app.post("/contas-a-receber", (req, res) => {
 try {
     
     if (!req.body.descricao) {
         return res.status(400).json({message: "A descrição da conta não foi informada, verifique" })
-    } 
+    }; 
     if (!req.body.valor) {
         return res.status(400).json({message: "O valor da conta não foi informado, verifique"})
-    }
+    };
     if (!req.body.data_de_competencia) {
-        return res.status(400).json({message: "A de competencia não foi informada, verifique"})
-    }
+        return res.status(400).json({message: "A data de competencia não foi informada, verifique"})
+    };
     if (!req.body.data_de_caixa) {
         return res.status(400).json({message: "A data de caixa não foi informada, verifique"})
-    }    
+    };    
     if (!req.body.contato) {
         return res.status(400).json({message: "O contato da conta não foi informada, verifique"})
-    } 
+    }; 
 
     const response = {
         descricao: req.body.descricao,
@@ -32,20 +31,20 @@ try {
         contato: req.body.contato
     };
 
-    fs.writeFile('teste.json', JSON.stringify(response, null, 2), (err) =>{
-        if (err) throw err;
+    fs.writeFile('teste.json', JSON.stringify(response, null, 2), () =>{
         return res.status(200).json({message: "Conta salva com sucesso!"})
-    })
+    });
 
 }   catch (error) {
         return res.status(500).json({message: "Erro ao tentar salvar o arquivo"})
-    }
+    };
 
 });
-
 app.get("/contas-a-receber/relatorios", (req, res) => {
     fs.readFile("teste.json", (err, arquivo) => {
-        if (err) throw err;
+    if (err) {
+        return res.status(400).json({message: "Relatório não existe"})
+    }
         let jeison = JSON.parse(arquivo)
         return res.status(200).json(jeison)
     })
